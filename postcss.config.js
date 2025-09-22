acd2eb8e101691add953be39697d13f7e16ce9f8
Ved Patel <vedevpatel@gmail.com>
2025-09-22 12:53:52 -0700

    Fixed dependency issues

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
+/** @type {import('postcss-load-config').Config} */
 module.exports = {
   plugins: {
-    "@tailwindcss/postcss7-compat": {},
-    autoprefixer: {},
-  },
-};
+    '@tailwindcss/postcss': {}
+  }
+}
